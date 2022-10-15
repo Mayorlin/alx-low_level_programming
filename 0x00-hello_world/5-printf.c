541e290d1faba0f9a42a410ef31c13bca0072753
Mayorlin <oyemomigabriel@gmail.com>
2022-10-15 15:06:32 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main _ Entry point
  *
- * Description: prints "with proper grammar, but the outcome is is a piece of 
+ * Description: prints "with proper grammar, but the outcome is a piece of 
  * art,"
  *
  * Return: Always 0 (Success)
