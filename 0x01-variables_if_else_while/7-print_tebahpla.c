43c4d917f6ef883d8058543a3f27b0662d8c601f
Mayorlin <oyemomigabriel@gmail.com>
2022-10-18 04:39:53 +0000

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - smile in the mirror
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
