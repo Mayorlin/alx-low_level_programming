ded520c120af47373b13338179cd8c5b76308db7
Mayorlin <oyemomigabriel@gmail.com>
2022-10-18 02:16:28 +0000

    insomia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - insomnia
+ *
+ * Description: Code to keep user awake
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	putchar("\n");
+	return (0);
+}
