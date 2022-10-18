a05d6a81f44153a67b4568ce98ee97bfedd98682
Mayorlin <oyemomigabriel@gmail.com>
2022-10-18 02:36:43 +0000

    print alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - upper and lowercase letters
+ *
+ * Description: How to print upper and lower case letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
