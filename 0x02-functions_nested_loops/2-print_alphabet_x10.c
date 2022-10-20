349d08a4146bc76692c8da47654903b1677d41f7
Mayorlin <oyemomigabriel@gmail.com>
2022-10-20 20:23:11 +0000

    issues

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints a to z 10x
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char lower_alpha;
+	char num_of_iteration;
+
+	lower_alpha = 'a';
+	num_of_iteration = 10;
+
+	while (num_of_iteration <= 10)
+	{
+		while (lower_alpha <= 'a')
+		{
+			_putchar(lower_alpha);
+			lower_alpha++;
+		}
+		_putchar('\n');
+
+		num_of_iteration++;
+	}
+}
