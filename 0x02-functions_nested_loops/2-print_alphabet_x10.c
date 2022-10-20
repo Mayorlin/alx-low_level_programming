a6db367f7222f1f021c1deac438a18ba7da420d6
Mayorlin <oyemomigabriel@gmail.com>
2022-10-20 20:38:41 +0000

    issues

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,10 +13,11 @@ void print_alphabet_x10(void)
 	char num_of_iteration;
 
 	lower_alpha = 'a';
-	num_of_iteration = 10;
 
 	while (num_of_iteration <= 10)
-	{
+	{	
+		num_of_iteration = 10;
+
 		while (lower_alpha <= 'a')
 		{
 			_putchar(lower_alpha);
