34e54bc1cd8ff7552e5dc6602270233d96fbf905
Mayorlin <oyemomigabriel@gmail.com>
2022-10-21 17:57:00 +0000

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Program to print two numbers
+ *
+ * Return: Always Return 0 Sucess
+ */
+
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+
+		if (c == '2' || c == '4')
+		{
+			c = c + 1;
+		}
+
+		c++;
+	}
+}
