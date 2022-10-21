8b1530637333e0408bac7e0e41a38bdc90f273a2
Mayorlin <oyemomigabriel@gmail.com>
2022-10-21 15:05:40 +0000

    Program

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for case of letter passe
+ *
+ * Description: c - accepts letters to determine its case
+ *
+ * Return: 0 always
+ */
+
+int _isupper(int c)
+{
+	if (isupper == c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
