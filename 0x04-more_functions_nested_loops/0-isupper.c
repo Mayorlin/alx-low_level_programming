632b3b3a1cc98e6cba8895fef06985dbe6975d2a
Mayorlin <oyemomigabriel@gmail.com>
2022-10-21 15:20:35 +0000

    issupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+int _isupper(int c);
+
 /**
  * _isupper - checks for case of letter passe
  *
@@ -11,7 +13,7 @@
 
 int _isupper(int c)
 {
-	if (isupper(c))
+	if ( isupper(c) )
 	{
 		return (1);
 	}
