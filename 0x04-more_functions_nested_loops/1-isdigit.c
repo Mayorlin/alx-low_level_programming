88ec62ec7fdccf3474345b6731a8347230d2068b
Mayorlin <oyemomigabriel@gmail.com>
2022-10-21 16:10:28 +0000

    _isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks the number
+ *
+ * @c: scans from 0 through 9
+ *
+ * Return: Always return 1 if successful or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
