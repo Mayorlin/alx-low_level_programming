61717a7496fd89d754553c456329c6f8c23c64ab
Mayorlin <oyemomigabriel@gmail.com>
2022-10-22 03:29:00 +0000

    print most programs

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,16 @@
 
 void more_numbers(void)
 {
-	char c;
-	char n;
-i
+	int c;
+	int n;
+
 	for (n = 1; n <= 10; n++)
 	{
 		for (c = 0; c <= 10; c++)
-		{	if (c >= 10)
+		{
+			if (c >= 10)
 			_putchar('1');
-			_putchar (C % 10 + '0');
+			_putchar (c % 10 +'0');
 		}
 		_putchar('\n');
 	}
