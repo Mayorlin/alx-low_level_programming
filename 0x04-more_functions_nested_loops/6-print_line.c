a9f656c5b3d21cf7ffa56facd597ecaa4cfba3c4
Mayorlin <oyemomigabriel@gmail.com>
2022-10-22 03:49:55 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 0; x <= n; x++)
+		_putchar(95);
+	_putchar('\n');
+}
