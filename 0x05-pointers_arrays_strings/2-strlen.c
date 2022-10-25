31c300fa6968b174187a6a72b2085f3c671ba5d0
Mayorlin <oyemomigabriel@gmail.com>
2022-10-25 14:06:19 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * _strlen - function that returns the length of a string
+ *
+ * @: returns the length of the string
+ *
+ * Return: Code sucessful when it returns an int
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	*s = length;
+}
