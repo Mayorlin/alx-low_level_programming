1b880220dfd3b9aaeebcdbc53a042c6c16b0ad0b
Mayorlin <oyemomigabriel@gmail.com>
2022-10-25 23:00:12 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Print reverse string
+ *
+ * @str: Pass pointer for string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s--;
+	}
+	-putchar('\n');
+}
