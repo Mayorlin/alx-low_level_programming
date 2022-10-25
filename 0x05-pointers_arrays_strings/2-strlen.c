3a4d27312e727fc068f0afc592b3edb9317a05e8
Mayorlin <oyemomigabriel@gmail.com>
2022-10-25 16:25:11 +0000

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * @_strlen - function that returns the length of a string
+/**
+ * _strlen - function that returns the length of a string
  *
  * @s: returns the length of the string
  *
