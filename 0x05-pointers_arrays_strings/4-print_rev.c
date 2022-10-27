32090e33ed3aa18171e9c65ec308160118b4cb69
Mayorlin <oyemomigabriel@gmail.com>
2022-10-27 00:28:35 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,21 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * print_rev - Print reverse string
  *
- * @str: Pass pointer for string
+ * @s: Pass pointer for string
  *
  * Return: void
  */
 
 void print_rev(char *s)
 {
+	int a, b;
 
-	for (; *s = '\0'; *s--)
+	a = strlen(s);
+	for (b = a - 1; b >= 0; b--)
 	{
-		_putchar(*s);
+		_putchar(s[b]);
 	}
 	_putchar('\n');
 }
