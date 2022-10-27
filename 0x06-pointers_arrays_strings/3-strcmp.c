f333f780abb875fede018cfd75112220ef6661de
Mayorlin <oyemomigabriel@gmail.com>
2022-10-27 13:54:21 +0000

    outcome

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - to compare
+ *
+ * @s1: first parameter
+ *
+ * @s2: Second parameter
+ *
+ * Return: int
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int outcome;
+
+	outcome = strcmp(s1, s2);
+	return (outcome);
+}
