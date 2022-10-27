9b58436cd38373c995b11fb565d1f459403e470d
Mayorlin <oyemomigabriel@gmail.com>
2022-10-27 01:15:21 +0000

    commit all changes

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - function that concat
+ *
+ * @dest: append string
+ *
+ * @src: string gets appended
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c;
+
+	c = strcat(dest, src);
+}
