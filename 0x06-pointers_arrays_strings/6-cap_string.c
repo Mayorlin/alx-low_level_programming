c28de64f4d3d014cc5a225a2ac3559913d5078fe
Mayorlin <oyemomigabriel@gmail.com>
2022-10-28 02:42:25 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *x)
 {
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?',
 		'"', '(', ')', '{', '}'};
-	int lent = 13;
+	int len = 13;
 	int a = 0;
 	int i;
 
@@ -22,9 +22,9 @@ char *cap_string(char *x)
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || x[a - 1] == spc[i] && (x[a] >= 97 && x[a] <= 122))
-					x[a] = x[a] - 32;
-				i++;
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				x[a] = x[a] - 32;
+			i++;
 		}
 		a++;
 	}
