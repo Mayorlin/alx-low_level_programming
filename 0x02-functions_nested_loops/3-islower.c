77f6e4f5b1decafc28af296dff0947b15affc30e
Mayorlin <oyemomigabriel@gmail.com>
2022-10-31 14:42:56 +0000

    functions and loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _islower(int c) - determines case
+ *
+ * @c: takes in the passed value
+ *
+ * Return: int
+ */
+
+int _islower(int c)
+{
+	if (!islower(c))
+		return (0);
+	else
+		return (1);
+}
