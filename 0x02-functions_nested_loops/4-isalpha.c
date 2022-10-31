8deadec89c97b4ddbc97631fa3fe03486c07dcc3
Mayorlin <oyemomigabriel@gmail.com>
2022-10-31 16:10:09 +0000

    isAlpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * int _isalpha - checks and return boolean value
+ *
+ * @c: passes value to function
+ *
+ */
+
+int _isalpha(int c)
+{
+	int upperCase;
+	int lowerCase;
+	
+	upperCase = 'A';
+	lowerCase = 'a';
+
+	while (upperCase = 'A')
+	{
+		if (c == upperCase)
+			return (1);
+			upperCase++;
+	}
+	while (lowerCase = 'a')
+	{
+		if (c == lowerCase)
+			return (1);
+		lowerCase++;
+	}
+
+		return (0);
+	return (0);
+}
