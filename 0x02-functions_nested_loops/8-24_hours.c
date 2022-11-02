9f7551510d65e5ae9256c2ed93f093b53e8f2a23
Mayorlin <oyemomigabriel@gmail.com>
2022-11-02 10:20:01 +0000

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day
+ *
+ * @h: hours
+ *
+ * @m: minutes
+ *
+ * @s: seconds
+ *
+ * Return: void
+ *
+ */
+
+void jack_bauer(void)
+{
+	int h;
+
+	h = "00";
+	while (h <= "24")
+	{
+		_putchar(h);
+
+		int m;
+
+		m = "00";
+			while (m <= "60")
+			{
+				_putchar("00: "m);
+
+				int s;
+
+				s = "00";
+					while (s <= "60")
+					{
+						_putchar ("00: 00: "s);
+						s++;
+					}
+				m++;
+			}
+		h++;
+	}
+}
+
